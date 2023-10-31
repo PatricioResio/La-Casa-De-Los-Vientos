a6e0a0a4927142f11a71f25ecd354fe1b2c820a2
Patricio Resio <patricio.resio@gmail.com>
2023-10-31 14:41:27 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Home from "./pages/home";
+
+function App() {
+  return (
+    <>
+      <h1>LA CASA DE LOS VIENTOS</h1>
+      <Home></Home>
+    </>
+  );
+}
+
+export default App;
